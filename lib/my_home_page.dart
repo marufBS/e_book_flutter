219c3edd0e831a607497310253744aaae82a92d5
Maruf Bin Solaiman <0909maruf@gmail.com>
2025-02-24 03:48:21 +0600

    setup added

---
+++ lib/my_home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+// import 'package:audio/app_colors.dart' as AppColors;
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
